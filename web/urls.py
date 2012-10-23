9738b35fca886193f55e27b37d3983b61a8f1d4d
Nicolas Malbran <nmalbran@gmail.com>
2012-10-23 19:15:47 -0300

    Added nosotros and unidades views.

--- web/urls.py
+++ web/urls.py
@@ -5,6 +5,9 @@ from views import NoticiaListView
 
 urlpatterns = patterns('',
     url(r'^/?$', redirect_to, {'url': '/noticias/aravarua'}),
-    url(r'^noticias/(?P<unidad_slug>[-\w\d]+)/?$', NoticiaListView.as_view(), name='noticias_unidad'),
+    url(r'^nosotros/?$', direct_to_template, {'template': 'static/nosotros.html'}),
+    url(r'^unidades/?$', direct_to_template, {'template': 'static/unidades.html'}),
+
+    url(r'^noticias/(?P<slug>[-\w\d]+)/?$', NoticiaListView.as_view(), name='noticias_unidad'),
 
 )
