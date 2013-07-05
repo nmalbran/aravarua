65ea4edd7c29544f8cbd16f45d759ed70de7fa43
Nicolas Malbran <nmalbran@gmail.com>
2013-07-05 00:22:30 -0400

    Added static serving from django.

--- aravarua/urls.py
+++ aravarua/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -13,4 +14,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
+)
+
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
